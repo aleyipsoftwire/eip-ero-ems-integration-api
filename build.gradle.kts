814b30abc72b40ab306398c270d5c5f522bafe15
Alex Yip <alex.yip@softwire.com>
2025-09-15 11:39:19 +0100

    EROPSPT-527: Test dependabot

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,7 +34,8 @@ allOpen {
 }
 
 val awsProfile = System.getenv("AWS_PROFILE_ARG") ?: "--profile code-artifact"
-val codeArtifactToken = "aws codeartifact get-authorization-token --domain erop-artifacts --domain-owner 063998039290 --query authorizationToken --output text $awsProfile".runCommand()
+val codeArtifactToken = System.getenv("AWS_CODEARTIFACT_TOKEN")
+// "aws codeartifact get-authorization-token --domain erop-artifacts --domain-owner 063998039290 --query authorizationToken --output text $awsProfile".runCommand()
 
 repositories {
     mavenCentral()
